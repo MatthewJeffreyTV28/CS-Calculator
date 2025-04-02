ce54146d8641b83d8a747eec6e8acd153190841b
MatthewJeffreyTV28 <mwjeffrey009@gmail.com>
2025-04-01 18:41:41 -0700

    Setp up C# folder, built 2 C# calculators

---
+++ Calc.cs
@@ -0,0 +1,39 @@
+﻿// using System;
+
+// namespace CS_Calculator
+// {
+//     class Calculator
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Choose your first number");
+//             int num1 = int.Parse(Console.ReadLine());
+//             Console.WriteLine("Choose and Operator. [+] [-] [*] [/] [%]");
+//             string operate = Console.ReadLine();
+//             Console.WriteLine("Choose your second number");
+//             int num2 = int.Parse(Console.ReadLine());
+//             string ans = "error";
+//             if (char.Parse(operate) == '+')
+//             {
+//                 ans = (num1 + num2).ToString();
+//             }
+//             else if (char.Parse(operate) == '-')
+//             {
+//                 ans = (num1 - num2).ToString();
+//             }
+//             else if (char.Parse(operate) == '*')
+//             {
+//                 ans = (num1 * num2).ToString();
+//             }
+//             else if (char.Parse(operate) == '/')
+//             {
+//                 ans = (num1 / num2).ToString();
+//             }
+//             else if (char.Parse(operate) == '%')
+//             {
+//                 ans = (num1 % num2).ToString();
+//             }
+//             Console.WriteLine("{0}: {1}" , "Answer" , ans);
+//         }
+//     }
+// }
---
+++ Calc2.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Other_CS_Cal
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your equation with spaces between each number and operator");
+            string[] input = Console.ReadLine().Split();
+            int num1 = int.Parse(input[0]);
+            int num2 = int.Parse(input[2]);
+            string ans = "error";
+            if (char.Parse(input[1]) == '+')
+            {
+                ans = (num1 + num2).ToString();
+            }
+            else if (char.Parse(input[1]) == '-')
+            {
+                ans = (num1 - num2).ToString();
+            }
+            else if (char.Parse(input[1]) == '*')
+            {
+                ans = (num1 * num2).ToString();
+            }
+            else if (char.Parse(input[1]) == '/')
+            {
+                ans = (num1 / num2).ToString();
+            }
+            else if (char.Parse(input[1]) == '%')
+            {
+                ans = (num1 % num2).ToString();
+            }
+            Console.WriteLine("{0}: {1}" , "Answer" , ans);
+
+
+        }
+    }
+}
